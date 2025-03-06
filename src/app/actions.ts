54158158f681638e173e3c266be4aa6beabe9717
Junrel Devocion <junrel.devocion.-nd@disney.com>
2025-03-06 10:14:22 +0800

    update sort

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -72,7 +72,7 @@ export const deleteDTR = async (formData: FormData) => {
 export const getDTR = async (id: string) => {
   await connectToMongoDB();
 
-  const result = DTR.findById(id);
+  const result = DTR.findById(id).sort('asc');
 
   if (!id || !result) {
     throw new Error('failed to fetch DTR');
