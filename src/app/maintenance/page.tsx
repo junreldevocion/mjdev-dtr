f097da152fbaff4b90a04b00bd0adf402f7d4633
Junrel Devocion <junrel.mdevocion@gmail.com>
2025-04-13 12:52:03 +0800

    added maintenance

---
+++ src/app/maintenance/page.tsx
@@ -0,0 +1,12 @@
+export default function MaintenancePage() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen text-center bg-gray-100 text-gray-800 px-4">
+      <h1 className="text-4xl font-bold mb-4">We&apos;ll Be Back Soon!</h1>
+      <p className="text-lg mb-2">
+        Our site is currently undergoing scheduled maintenance.
+        We apologize for the inconvenience and appreciate your patience.
+      </p>
+      <p className="text-lg">Thank you for understanding!</p>
+    </div>
+  );
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,27 +5,48 @@ import { decrypt } from './lib/session'
 
 const protectedRoutes = ['/home']
 const publicRoutes = ['/signin', '/signup']
+const maintenanceRoutes = ['/maintenance']
+
+const shutdownText = process.env.SHUTDOWN
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
 
   const path = req.nextUrl.pathname
 
+  const isShutdown = shutdownText === 'true'
+
+  const isMaintenanceRoute = maintenanceRoutes.includes(path)
+
   const isProtectedRoute = protectedRoutes.includes(path)
   const isPublicRoute = publicRoutes.includes(path)
+
+  if (isShutdown && (isProtectedRoute || isPublicRoute)) {
+    console.log('Server is shutting down')
+    return NextResponse.redirect(new URL('/maintenance', req.nextUrl))
+  }
 
   const cookie = (await cookies()).get('session')?.value
   const session = await decrypt(cookie)
 
 
-  if (isProtectedRoute && !session?.userId) {
+  if (isProtectedRoute && !session?.userId && !isShutdown) {
     return NextResponse.redirect(new URL('/signin', req.nextUrl))
+  }
+
+  if (isMaintenanceRoute && !session?.userId && !isShutdown) {
+    return NextResponse.redirect(new URL('/signin', req.nextUrl))
+  }
+
+  if (isMaintenanceRoute && session?.userId && !path.startsWith('/home')) {
+    return NextResponse.redirect(new URL('/home', req.nextUrl))
   }
 
   if (
     isPublicRoute &&
     session?.userId &&
     !path.startsWith('/home')
+    && !isShutdown
   ) {
     return NextResponse.redirect(new URL('/home', req.nextUrl))
   }
